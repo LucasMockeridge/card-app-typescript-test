95d91af57f6f886cdbfb05ffd1976a8b1b598fef
Lucas Mockeridge <lucasmockeridge@gmail.com>
2024-10-25 10:26:23 +0100

    Settings button

---
+++ frontend/src/components/Settings.tsx
@@ -0,0 +1,5 @@
+export default function Settings(){
+        return(
+                        <button className="m-3 p-4 text-xl bg-blue-400 hover:bg-blue-500 rounded-md font-medium text-white">Settings</button>
+        )
+}
