0d7e7befc33450003f5204af7835d673f6fd755b
Lucas Mockeridge <lucasmockeridge@gmail.com>
2024-10-25 10:40:12 +0100

    Basic dialog

--- frontend/src/components/Settings.tsx
+++ frontend/src/components/Settings.tsx
@@ -1,5 +1,29 @@
-export default function Settings(){
-        return(
-                        <button className="m-3 p-4 text-xl bg-blue-400 hover:bg-blue-500 rounded-md font-medium text-white">Settings</button>
-        )
+import { useRef } from "react";
+
+export default function Settings() {
+	
+	const settingsRef = useRef<HTMLDialogElement>(null)
+
+        function toggleSettings(){
+                if(settingsRef.current){
+                        if (settingsRef.current.hasAttribute("open")){
+                                settingsRef.current.close()
+                        }
+                        else{
+                                settingsRef.current.showModal()
+                        }
+                }
+        }
+	
+  	return (
+		<>
+			<button className="m-3 p-4 text-xl bg-blue-400 hover:bg-blue-500 rounded-md font-medium text-white" onClick={toggleSettings}>
+      				Settings
+    			</button>
+			<dialog onClick={toggleSettings} ref={settingsRef}>
+           	 		<h1>Settings</h1>
+       			</dialog>
+
+		</>
+ 	 );
 }
