5211efbd7a81780908544235c1ee547382fc7f8a
Lucas Mockeridge <lucasmockeridge@gmail.com>
2024-10-25 10:49:49 +0100

    Dark mode checkbox

--- frontend/src/components/Settings.tsx
+++ frontend/src/components/Settings.tsx
@@ -1,29 +1,36 @@
 import { useRef } from "react";
 
 export default function Settings() {
-	
-	const settingsRef = useRef<HTMLDialogElement>(null)
+  const settingsRef = useRef<HTMLDialogElement>(null);
 
-        function toggleSettings(){
-                if(settingsRef.current){
-                        if (settingsRef.current.hasAttribute("open")){
-                                settingsRef.current.close()
-                        }
-                        else{
-                                settingsRef.current.showModal()
-                        }
-                }
-        }
-	
-  	return (
-		<>
-			<button className="m-3 p-4 text-xl bg-blue-400 hover:bg-blue-500 rounded-md font-medium text-white" onClick={toggleSettings}>
-      				Settings
-    			</button>
-			<dialog onClick={toggleSettings} ref={settingsRef}>
-           	 		<h1>Settings</h1>
-       			</dialog>
+  function toggleSettings() {
+    if (settingsRef.current) {
+      if (settingsRef.current.hasAttribute("open")) {
+        settingsRef.current.close();
+      } else {
+        settingsRef.current.showModal();
+      }
+    }
+  }
 
-		</>
- 	 );
+  return (
+    <>
+      <button
+        className="m-3 p-4 text-xl bg-blue-400 hover:bg-blue-500 rounded-md font-medium text-white"
+        onClick={toggleSettings}
+      >
+        Settings
+      </button>
+      <dialog onClick={(e) => {
+		if(e.currentTarget === e.target){
+			toggleSettings()
+		}}} ref={settingsRef}>
+		<div className="grid grid-rows-2 justify-items-center gap-3">
+            		<h1 className="col-span-2">Settings</h1>
+      			<input className="row-start-2" id="dark-mode" type="checkbox" />
+          		<label className="row-start-2" htmlFor="dark-mode">Dark Mode</label>
+            	</div>
+      </dialog>
+    </>
+  );
 }
